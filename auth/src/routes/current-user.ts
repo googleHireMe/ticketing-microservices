535776c97595a833797d05871e4874d3881cb31a
Nikita Nevokshonov <nnevokshonov@aligntech.com>
2021-08-11 19:50:04 +0300

    common npm introduced for funtionality  sharing

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
+import { currentUser, requireAuth } from '@nicknevokshonov/common';
 
 const router = Router();
 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { body } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
-import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError, validateRequest } from '@nicknevokshonov/common';
 import { User } from '../models/user';
 import { Password } from '../services/password';
 import jwt from 'jsonwebtoken';
