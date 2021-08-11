6075eab2faa9e7f7cec1a50f8c81e9b6d721a03d
Nikita Nevokshonov <nnevokshonov@aligntech.com>
2021-08-11 16:27:29 +0300

    signup + mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
 
 const app = express();
 app
@@ -15,16 +16,25 @@ app
 	.use(signinRouter)
 	.use(signoutRouter)
 	.use(signupRouter)
-	.use(errorHandler);
+	.use(errorHandler)
+	.all('*', async (req, res) => {
+		throw new NotFoundError();
+	});
 
-app.get('/', (res, req) => {
-	req.send({ 'test': true });
-});
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		});
+		console.log('Connected to MongoDB');
+	} catch (err) {
+		console.error(err);
+	}
+	app.listen(3000, () => {
+		console.log('Listening on port 3000');
+	});
+}
 
-app.all('*', async (req, res) => {
-	throw new NotFoundError();
-});
-
-app.listen(3000, () => {
-	console.log('Listening on port 3000');
-});
+start();
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,12 +3,12 @@ import { CustomError } from '../errors/custom-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 
-export function errorHandler(
+export const errorHandler = (
 	err: Error,
 	req: Request,
 	res: Response,
 	next: NextFunction
-) {
+) => {
 	if (err instanceof CustomError) {
 		res.status(err.statusCode ).send({ message: err.serializeErrors() });
 	}
